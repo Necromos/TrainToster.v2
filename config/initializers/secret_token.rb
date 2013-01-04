e4056ee09dbd330da04166f3bfd5e987bde96fe6
Necromos <masternecromos@gmail.com>
2013-01-04 18:28:07 +0100

    'init'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrainTosterV2::Application.config.secret_token = '60f75c8a477a6eb79b6b2b966b08809b38dfac731a0e43ec110930e9fd5a82abc3de57f5b1a471c20d2237ac68fbcc14cfe45ff6a8cc6bc63431cd5dfcb48a18'
