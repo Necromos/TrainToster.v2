96010eefe02d4d72f3ff2f88df3c3fb177f25a3a
Necromos <masternecromos@gmail.com>
2013-01-07 20:07:34 +0100

    zmiana route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ TrainTosterV2::Application.routes.draw do
   
   resources :trains
 
-  root to: 'train#index'
+  root to: 'trains#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
